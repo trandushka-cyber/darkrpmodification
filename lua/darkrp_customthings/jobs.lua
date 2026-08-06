1ad0df8d600ebafa0390a899a6ae269ce61ad09e
trandushka-cyber <trandushka@gmail.com>
2026-08-06 16:19:48 +0300

    Update jobs.lua

--- lua/darkrp_customthings/jobs.lua
+++ lua/darkrp_customthings/jobs.lua
@@ -35,3 +35,17 @@ GAMEMODE.CivilProtection = {
 Jobs that are hitmen (enables the hitman menu)
 ---------------------------------------------------------------------------]]
 DarkRP.addHitmanTeam(TEAM_MOB)
+41 = DarkRP.createJob("Полицай", {
+	color = Color(30, 100, 160, 255),
+	model = {"models/player/combine_super_soldier.mdl"},
+	description = [[Мусор]],
+	weapons = {"m9k_m14sp"},
+	command = "",
+	max = 4,
+	salary = 100,
+	admin = 0,
+	vote = false,
+	hasLicense = true,
+	candemote = true,
+	category = "Civil Protection",
+})
